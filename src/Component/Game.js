4c755b1e8e93963d781120b21a30582d0eaf5de7
Waseem Akram <waseemakm07@gmail.com>
2023-01-18 22:24:34 +0530

    first commit

---
+++ src/Component/Game.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Game extends React.Component {
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+export default Game;
---
+++ src/Component/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Square extends React.Component {
+
+    render() {
+        return(
+            <div> </div>
+        )
+    }
+}
+
+export default Square;
