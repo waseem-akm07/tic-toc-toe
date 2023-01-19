4d30eb741c536924d4dfaaa2284bbc8db96ec501
Waseem Akram <waseemakm07@gmail.com>
2023-01-19 21:27:30 +0530

    Added cases to jump on move and stepnumber

--- src/Component/Board.js
+++ src/Component/Board.js
@@ -2,43 +2,20 @@ import React from "react";
 import Sqaure from "./Square";
 
 class Board extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        }
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.splice();
-        if(CalculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O'
-        this.setState({squares: squares, xIsNext: !this.state.xIsNext})
-    }
-
+   
     renderSquare(i){
         return(
             <Sqaure 
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
                 />
         );
     }
 
     render() {
-        const winner = CalculateWinner(this.state.squares);
-        let status;
-        if(winner)
-            status = "Winner: " + winner;
-        else
-            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O') ;
-
         return (
             <div>
-                <div className="status">{status}</div>
+                <div className="status"></div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -59,24 +36,4 @@ class Board extends React.Component {
     }
 }
 
-function CalculateWinner(squares) {
-    const line = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-
-    for(let i = 0; i < line.length; i++){
-        const [a, b, c] = line[i];
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-            return squares[a];
-        }
-    }
-    return null;
-}
 export default Board;
